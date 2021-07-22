c91bae60d9b06167ab3b9c619c08b3b948709da0
Galen Rowell <growell3@gmail.com>
2021-07-22 22:05:27 +1000

    adding latex/line # utils & enable statusline with terminals

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,9 +1,14 @@
 -- uncomment this if you want to open nvim with a dir
 -- vim.cmd [[ autocmd BufEnter * if &buftype != "terminal" | lcd %:p:h | endif ]]
 
--- Use relative & absolute line numbers in 'n' & 'i' modes respectively
--- vim.cmd[[ au InsertEnter * set norelativenumber ]]
--- vim.cmd[[ au InsertLeave * set relativenumber ]]
+-- User relative & absolutely line numbers in 'n' & 'i' modes respectively
+vim.cmd [[ au InsertEnter * set norelativenumber ]]
+vim.cmd [[ au InsertLeave * set relativenumber ]]
+
+-- Latex utilities, auto. compile on save, auto. clean on exit
+vim.cmd [[ au BufEnter,BufRead,BufNewFile *.tex :set spell filetype=tex ]]
+vim.cmd [[ au BufWritePost *.tex silent !compiler.sh % ]]
+vim.cmd [[ au BufDelete,VimLeave *.tex silent !texclear.sh % ]]
 
 -- Don't show any numbers inside terminals
 vim.cmd [[ au TermOpen term://* setlocal nonumber norelativenumber | setfiletype terminal ]]
