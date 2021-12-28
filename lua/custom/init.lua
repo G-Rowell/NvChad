8e63786fafe6fb0180c09e28d35109bca5faec25
Galen Rowell <growell3@gmail.com>
2021-12-28 17:28:39 +1100

    custom config start

---
+++ lua/custom/init.lua
@@ -0,0 +1,43 @@
+-- This is an example init file , its supposed to be placed in /lua/custom dir
+-- lua/custom/init.lua
+
+-- This is where your custom modules and plugins go.
+-- Please check NvChad docs if you're totally new to nvchad + dont know lua!!
+
+local hooks = require "core.hooks"
+
+-- Use relative & absolute line numbers in 'n' & 'i' modes respectively
+vim.cmd[[ au InsertEnter * set norelativenumber ]]
+vim.cmd[[ au InsertLeave * set relativenumber ]]
+
+-- MAPPINGS
+-- To add new plugins, use the "setup_mappings" hook,
+
+-- hooks.add("setup_mappings", function(map)
+--    map("n", "<leader>cc", ":Telescope <CR>", opt)
+--    map("n", "<leader>q", ":q <CR>", opt)
+-- end)
+
+-- NOTE : opt is a variable  there (most likely a table if you want multiple options),
+-- you can remove it if you dont have any custom options
+
+-- Install plugins
+-- To add new plugins, use the "install_plugin" hook,
+
+-- examples below:
+
+-- hooks.add("install_plugins", function(use)
+--    use {
+--       "max397574/better-escape.nvim",
+--       event = "InsertEnter",
+--    }
+--
+--    use {
+--       "user or orgname/reponame",
+--       --further packer options
+--    }
+-- end)
+
+-- NOTE: we heavily suggest using Packer's lazy loading (with the 'event' field)
+-- see: https://github.com/wbthomason/packer.nvim
+-- https://nvchad.github.io/config/walkthrough
